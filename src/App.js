85b5a3fca82051d86be5610dc1ae13bdb37788cf
Daniel Velasquez <apu.danielvelasquez@gmail.com>
2020-06-14 18:52:29 -0300

    Agregue card base

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from "react";
 import "./App.css";
+import Card from "./components/Card";
 
 function App() {
   return (
     <div className="App container">
       <div className="row">
         <div className="col-sm-4">col-sm-4</div>
-        <div className="col-sm-8">col-sm-8</div>
+        <div className="col-sm-8">
+          <Card />
+        </div>
       </div>
     </div>
   );
