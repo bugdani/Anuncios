0e6c6bcfe8f4c55692b2c2f44a86c2a88695fa03
Daniel Velasquez <apu.danielvelasquez@gmail.com>
2020-06-14 19:56:43 -0300

    Consumo api que devuelve las publicaciones

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import useFetch from "./hooks/useFetch";
+import { URL_API_POSTINGS } from "./utils/constants";
 import "./App.css";
 import Card from "./components/Card";
 
 function App() {
+  const postingsResult = useFetch(URL_API_POSTINGS, null);
+  const [postings, setPostings] = useState([]);
+
+  console.log(postingsResult);
   return (
-    <div className="App container">
+    <div className="App container mt-5">
       <div className="row">
         <div className="col-sm-4">col-sm-4</div>
         <div className="col-sm-8">
+          <Card />
+          <Card />
           <Card />
         </div>
       </div>
