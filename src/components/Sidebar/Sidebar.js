2fe16e458adbfbd92fde85b42cace07188e5dfc4
Daniel Velasquez <apu.danielvelasquez@gmail.com>
2020-06-15 01:17:32 -0300

    Agrego componente collapse para sidebar

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,6 +1,35 @@
 import React from "react";
 import "./Sidebar.scss";
+import { ReactComponent as Search } from "../../assets/img/buscar.svg";
+import { Accordion, Card } from "react-bootstrap";
 
 export default function Sidebar() {
-  return "";
+  return (
+    <>
+      <Card>
+        <Card.Body>
+          <Accordion defaultActiveKey="0">
+            <Card>
+              <Accordion.Toggle as={Card.Header} eventKey="0">
+                Direccion
+              </Accordion.Toggle>
+              <Accordion.Collapse eventKey="0">
+                <Card.Body>Hello! I'm the body</Card.Body>
+              </Accordion.Collapse>
+            </Card>
+          </Accordion>
+          <Accordion defaultActiveKey="0">
+            <Card>
+              <Accordion.Toggle as={Card.Header} eventKey="0">
+                Direccion
+              </Accordion.Toggle>
+              <Accordion.Collapse eventKey="0">
+                <Card.Body>Hello! I'm the body</Card.Body>
+              </Accordion.Collapse>
+            </Card>
+          </Accordion>
+        </Card.Body>
+      </Card>
+    </>
+  );
 }
