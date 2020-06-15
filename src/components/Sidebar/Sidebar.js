ac97bb64ee37b4ff26cbdb3ecbbb92f08d3e4822
Daniel Velasquez <apu.danielvelasquez@gmail.com>
2020-06-15 17:48:08 -0300

    arreglando estilo de sidebar

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,12 +1,12 @@
 import React from "react";
 import "./Sidebar.scss";
 import { ReactComponent as Search } from "../../assets/img/buscar.svg";
-import { Accordion, Card, Form } from "react-bootstrap";
+import { Accordion, Card, Form, Col } from "react-bootstrap";
 
 export default function Sidebar() {
   return (
     <>
-      <Card className="sidebar .float-right" style={{ maxWidth: 300 }}>
+      <Card className="sidebar p-2" style={{ maxWidth: 300, maxHeight: 450 }}>
         <Card.Body className="sidebar__body text-left">
           <Card.Text className="sidebar__body-title">Filtrado actual</Card.Text>
           <hr />
@@ -20,68 +20,61 @@ export default function Sidebar() {
                 Direccion
               </Accordion.Toggle>
               <Accordion.Collapse eventKey="0">
-                <Card.Body>
-                  <div className="form-row">
-                    <div className="col">
-                      <input
-                        className="form-control sidebar__body__search__input"
-                        type="text"
-                        placeholder="Buscar por direccion"
-                      />
-                    </div>
-                    <div className="col">
-                      <button
-                        type="submit"
-                        className="btn btn-light sidebar__body__search__button"
-                      >
-                        <Search className="sidebar__body__search__button-icon" />
-                      </button>
-                    </div>
+                <div className="form-row">
+                  <div className="col">
+                    <input
+                      className="form-control sidebar__body__search__input"
+                      type="text"
+                      placeholder="Buscar por direccion"
+                    />
+                    <button
+                      type="submit"
+                      className="btn btn-light sidebar__body__search__button"
+                    >
+                      <Search className="sidebar__body__search__button-icon" />
+                    </button>
                   </div>
-                </Card.Body>
+                </div>
               </Accordion.Collapse>
             </Card>
           </Accordion>
+          <hr />
           <Accordion defaultActiveKey="0" className="">
             <Card className="border-0">
-              <Accordion.Toggle as={Card.Text} eventKey="0">
+              <Accordion.Toggle
+                as={Card.Text}
+                eventKey="0"
+                className="sidebar__body__type-operation__title"
+              >
                 Tipo de operacion
               </Accordion.Toggle>
               <Accordion.Collapse eventKey="0">
-                <Card.Body>
-                  <div key={`custom-radio`} className="mb-3">
-                    <Form.Check
-                      custom
-                      type={"radio"}
-                      id={`custom-radio1`}
-                      label={`Check this custom radio`}
-                    />
-                  </div>
-                  <div key={`custom-radio`} className="mb-3">
-                    <Form.Check
-                      custom
-                      type={"radio"}
-                      id={`custom-radio2`}
-                      label={`Check this custom radio`}
-                    />
-                  </div>
-                  <div key={`custom-radio`} className="mb-3">
-                    <Form.Check
-                      custom
-                      type={"radio"}
-                      id={`custom-radio3`}
-                      label={`Check this custom radio`}
-                    />
-                  </div>
-                  <div key={`custom-radio`} className="mb-3">
-                    <Form.Check
-                      custom
-                      type={"radio"}
-                      id={`custom-radio4`}
-                      label={`Check this custom radio`}
-                    />
-                  </div>
-                </Card.Body>
+                <Col sm={10}>
+                  <Form.Check
+                    type="radio"
+                    label="Comprar"
+                    name="formHorizontalRadios"
+                    id="formHorizontalRadios1"
+                  />
+                  <Form.Check
+                    type="radio"
+                    label="Alquilar"
+                    name="formHorizontalRadios"
+                    id="formHorizontalRadios2"
+                  />
+                  <Form.Check
+                    type="radio"
+                    label="Temporal"
+                    name="formHorizontalRadios"
+                    id="formHorizontalRadios3"
+                  />
+                  <Form.Check
+                    type="radio"
+                    label="Todos"
+                    name="formHorizontalRadios"
+                    id="formHorizontalRadios3"
+                  />
+                </Col>
               </Accordion.Collapse>
             </Card>
           </Accordion>
