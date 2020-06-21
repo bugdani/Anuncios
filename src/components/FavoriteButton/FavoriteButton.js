3f14e2daeeae8f180bdc2c773c2d76898dfb2217
Daniel Velasquez <apu.danielvelasquez@gmail.com>
2020-06-21 18:49:55 -0300

    Borro archivos y lineas de código innecesarias

--- src/components/FavoriteButton/FavoriteButton.js
+++ src/components/FavoriteButton/FavoriteButton.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import "./FavoriteButton.scss";
 import { ReactComponent as Favorite } from "../../assets/img/corazon.svg";
 
--- src/utils/getIndex.js
+++
@@ -1,9 +0,0 @@
-export const getIndex = (allConfigurationArray, posting_id) => {
-  let indexP;
-  allConfigurationArray.forEach((element, index) => {
-    if (element.id === posting_id) {
-      indexP = index;
-    }
-  });
-  return indexP;
-};
